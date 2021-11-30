8b8b93709ace67a8b5a0150ec7c8b840149a4f81
Sylas <38060892+glacierheavner@users.noreply.github.com>
2021-11-30 12:14:39 -0700

    Created choose command

---
+++ commands/choose.js
@@ -0,0 +1,55 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const helpers = require("../helpers");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("choose")
+        .setDescription("Choose between some options!")
+        .addStringOption(option => option
+            .setName("options")
+            .setDescription("The options to choose from, separated by \", \"")
+            .setRequired(true))
+        .addBooleanOption(option => option
+            .setName("hidden")
+            .setDescription("Whether my choice should be shown to everyone or just you")
+            .setRequired(false)),
+
+    async execute(interaction) {
+        const options = interaction.options.getString("options");
+        const hidden = interaction.options.getBoolean("hidden") || false;
+
+        let splitOptions = options.split(", ");
+
+        if (splitOptions.length === 1) {
+            return interaction.reply({
+                embeds: [
+                    helpers.createEmbed({
+                        title: "Whoops!",
+                        description: "You must provide at least two options!"
+                    })
+                ],
+                ephemeral: true
+            });
+        }
+
+        let choice = splitOptions[Math.floor(Math.random() * splitOptions.length)];
+
+        interaction.reply({
+            embeds: [
+                helpers.createEmbed({
+                    title: choice,
+                    description: `I choose ${choice}!`,
+                    fields: [
+                        {
+                            name: "Options You Gave",
+                            value: splitOptions.join("\n")
+                        }
+                    ]
+                })
+            ],
+            ephemeral: hidden
+        });
+
+        return console.log(`${interaction.user.username} made a choice!`);
+    }
+}
