7d8da250bfbff9981c01c8e1f82130fbf2c459df
Sylas <38060892+glacierheavner@users.noreply.github.com>
2021-12-05 17:53:09 -0700

    Updated /ping to use json data

--- commands/ping.js
+++ commands/ping.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
+const { pingResponses } = require("../data/misc/random_messages.json");
 const helpers = require("../helpers");
 
 module.exports = {
@@ -7,21 +8,11 @@ module.exports = {
         .setDescription("Check if I'm online!"),
 
     async execute(interaction) {
-        const responses = [
-            "I'm just tending to the roses!",
-            "Sorry, I found a butterfly and wandered off!",
-            "Don't worry, I'm here!",
-            "Come check out this flower!",
-            "I just saw the prettiest little bug!",
-            "Check it out, I found a cool rock!",
-            "I must have dozed off in the shade!"
-        ];
-
         interaction.reply({
             embeds: [
                 helpers.createEmbed({
                     title: "Pong!",
-                    description: responses[Math.floor(Math.random() * responses.length)],
+                    description: pingResponses[Math.floor(Math.random() * pingResponses.length)],
                     author: interaction.user
                 })
             ]
